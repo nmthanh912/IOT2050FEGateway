790375bf6346573a99de1c14e0e2cc53747b2848
phucvinh57 <npvinh0507@gmail.com>
2022-08-05 15:00:57 +0700

    remove handleCreateErr and setupTagSql

--- NodejsApp/controllers/device.controller.js
+++ NodejsApp/controllers/device.controller.js
@@ -6,45 +6,6 @@ const { uniqueId, renameObjectKey, logError, handler } = require("../utils")
 const deviceModel = require("../models/device.model");
 
 class DeviceController {
-	handleErrCreate = async (id) => {
-		const getDevice = `SELECT * FROM DEVICE WHERE ID = ?`;
-		const device = await dbAll(getDevice, id);
-
-		if (device) {
-			const deleteQuery = `DELETE FROM DEVICE WHERE ID = ?`;
-			await dbRun(deleteQuery, id);
-		}
-	};
-
-	setupTagSql = (tagList, protocolName, id) => {
-		tagList.forEach(tag => {
-			tag.deviceID = id;
-		});
-
-		const bracketValue =
-			"(" + ",?".repeat(Object.values(tagList[0]).length).slice(1) + "), ";
-		const proTagQuery =
-			`INSERT INTO ${protocolName}_TAG VALUES ` +
-			bracketValue.repeat(tagList.length).slice(0, -2);
-
-		const proTagParams = tagList.map(tag => Object.values(tag)).flat();
-
-		const bracketValueTag = "(" + ",?".repeat(2).slice(1) + "), ";
-		const tagQuery =
-			`INSERT INTO TAG VALUES ` +
-			bracketValueTag.repeat(tagList.length).slice(0, -2);
-		const values = [];
-		tagList.forEach(tag =>
-			values.push({
-				deviceID: tag.deviceID,
-				name: tag.name,
-			})
-		);
-		const tagParams = values.map((value) => Object.values(value)).flat();
-
-		return { proTagQuery, proTagParams, tagQuery, tagParams };
-	};
-
 	getAll = async function (req, res) {
 		try {
 			const devices = await deviceModel.getAll();
@@ -53,8 +14,8 @@ class DeviceController {
 			));
 			res.json(data);
 		} catch (err) {
-			// Log message
-			res.status(INTERNAL_SERVER_ERROR_CODE).json({ msg: "Query fail" })
+			logError(err.message)
+			res.status(INTERNAL_SERVER_ERROR_CODE).json({ msg: err.message })
 		}
 	};
 
@@ -64,7 +25,7 @@ class DeviceController {
 			res.status(BAD_REQUEST_CODE).json({ msg: INVALID_REPLICATE_NUMBER })
 			return
 		}
-		
+
 		const protocolName = deviceData.protocol.toUpperCase()
 		try {
 			const keyList = replicateNumber > 1 ? await deviceModel.createMany(
